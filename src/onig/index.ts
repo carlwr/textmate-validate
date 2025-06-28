35482e1a174f2ecfc1328ffd82f9d8133e038529
Carl <carl@wernhoff.com>
2025-06-28 20:53:23 +0200

    refactor(onig/index.ts): use readFile from fs/promises

--- src/onig/index.ts
+++ src/onig/index.ts
@@ -1,10 +1,15 @@
-import { readFileSync } from 'node:fs'
+import { readFile as readFilePromise} from 'node:fs/promises'
 import type * as vscTM from 'vscode-textmate'
 import { loadVscOnig } from './loader.js'
 import { getOnigWasmPath } from './wasmPath.js'
 
 export async function getVscOnigFixed(): Promise<vscTM.IOnigLib> {
   const onigPath = getOnigWasmPath()
-  const wasmData = readFileSync(onigPath).buffer
+  const wasmData = await readFileAsArrayBuffer(onigPath)
   return await loadVscOnig(wasmData)
+}
+
+async function readFileAsArrayBuffer(filePath: string): Promise<ArrayBuffer> {
+  const fileData = await readFilePromise(filePath)
+  return new Blob([new Uint8Array(fileData)]).arrayBuffer()
 }
